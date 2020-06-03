84dbbbeefee39b6ebcd6430830b2bf165a691d81
othman19t <othman19t@gmail.com>
2020-06-03 16:28:59 -0700

    create store and get state on that store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+import { createStore } from "redux"; // npm i redux
+
+//create store 
+const store = createStore((state = { count: 0 }) => {
+  return state;
+});
+console.log(store.getState()); // to get the state on the store
+
 function App() {
   return (
     <div className="App">
